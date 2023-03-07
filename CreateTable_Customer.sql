e791207567abd73779bc3cffe017f756ff7b0dd6
septianitrifosa <122073129+septianitrifosa@users.noreply.github.com>
2023-03-07 18:21:45 +0700

    Update CreateTable_Customer.sql

--- CreateTable_Customer.sql
+++ CreateTable_Customer.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Customer(
     Custno char(8)CONSTRAINT cust_custno_pk PRIMARY KEY,
     CustFirstName varchar2(15), 
-    CustLasName Varchar2(15), 
+    CustLastName Varchar2(15), 
     CustStreet Varchar2(30), 
     CustCity Varchar(15), 
     CustState char(2), 
@@ -65,3 +65,122 @@ CREATE TABLE DOrder(
 	ProdNo Char(8),
 	Qty Number(3));
 
+--ADD PK
+ALTER TABLE DOrder
+    ADD CONSTRAINT do_orderno_prodno_pk PRIMARY KEY(Prodno,OrdNo);
+describe DOrder;
+
+--ADD FK
+--ADD FK CUSTNO
+ALTER TABLE dorder add constraint ord_ordno_fk
+FOREIGN KEY(custno) REFERENCES customer(custno);
+    
+--AD FK EMPNO
+ALTER TABLE morder add constraint ord_empno_fk
+FOREIGN KEY(empno) REFERENCES salesman(empno);
+
+
+--INSERT RECORD
+INSERT INTO Customer (
+    Custno, 
+    CustFirstname, 
+    CustLastname, 
+    Custstreet, 
+    Custcity, 
+    CustState, 
+    CustZip, 
+    Custbal)
+VALUES (
+    'C0954327', 
+    'Sherri', 
+    'Gordon', 
+    '336 Hill St.', 
+    'Littleton', 
+    'CO', 
+    '80129-5543', 
+    '230');
+
+INSERT INTO Customer (
+    Custno, 
+    CustFirstname, 
+    CustLastname, 
+    Custstreet, 
+    Custcity, 
+    CustState, 
+    CustZip, 
+    Custbal)
+VALUES (
+    'C1010398',
+	'Jim',
+    'Glussman',
+	'1432 E.Ravenna',
+	'Denver',
+	'CO',
+	'80111-0033',
+	'200');
+
+INSERT INTO Customer (
+    Custno, 
+    CustFirstname, 
+    CustLastname, 
+    Custstreet, 
+    Custcity, 
+    CustState, 
+    CustZip, 
+    Custbal)
+VALUES (
+    'C2388597',
+	'Beth',
+    'Taylor',
+	'2396 Rafter Rd',
+	'Seattle',
+	'WA',
+	'98103-1121',
+	'500');
+
+INSERT INTO Customer (
+    Custno, 
+    CustFirstname, 
+    CustLastname, 
+    Custstreet, 
+    Custcity, 
+    CustState, 
+    CustZip, 
+    Custbal)
+VALUES (
+    'C3340959',
+	'Betty',
+    'Wise',
+	'4334 153rd NW',
+	'Seattle',
+	'WA',
+	'98178-331',
+	'200');
+
+INSERT INTO Customer (
+    Custno, 
+    CustFirstname, 
+    CustLastname, 
+    Custstreet, 
+    Custcity, 
+    CustState, 
+    CustZip, 
+    Custbal)
+VALUES (
+    'C3499503',
+	'Bob',
+    'Mann',
+	'1190 Lorraine Cir.',
+	'Monroe',
+	'WA',
+	'98013-1095',
+	'0');
+
+select*from customer;
+
+--UPDATE DATA
+UPDATE Customer SET CustFirstName ='Sherry'
+WHERE Custno = 'C0954327';
+
+UPDATE Customer SET CustBal = 100
+WHERE CustFirstName = 'Bob';;
