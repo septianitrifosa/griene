1817c96dc9bd0b0e334b004c0aea940a0b8e25c4
septianitrifosa <122073129+septianitrifosa@users.noreply.github.com>
2023-03-07 16:36:49 +0700

    Update CreateTable_Customer.sql

--- CreateTable_Customer.sql
+++ CreateTable_Customer.sql
@@ -1,5 +1,6 @@
+--CUSTOMER
 CREATE TABLE Customer(
-    Custno char(8), 
+    Custno char(8)CONSTRAINT cust_custno_pk PRIMARY KEY,
     CustFirstName varchar2(15), 
     CustLasName Varchar2(15), 
     CustStreet Varchar2(30), 
@@ -7,35 +8,60 @@ CREATE TABLE Customer(
     CustState char(2), 
     CustZip Char(10), 
     CustBal Number(6,2));
-
 DESCRIBE Customer;
-
---ADD
-ALTER TABLE Customer ADD Telepon varchar2(15);
-DESCRIBE Customer;
-
---MODIFY
-ALTER TABLE Customer MODIFY Telepon varchar2(12);
-DESCRIBE Customer;
-
---RENAME
-ALTER TABLE Customer RENAME COLUMN Telepon TO noTlp;
-DESCRIBE Customer;
-
---DROP COLUMN
-ALTER TABLE Customer DROP COLUMN noTlp;
-DESCRIBE Customer;
-
---ADD PK
-ALTER TABLE Customer 
-    ADD CONSTRAINT cust_custno_pk PRIMARY KEY(custno);
-DESCRIBE Customer;
-
---ADD FK
 
 --MENAMPILKAN CONSTRAINT
 SELECT * FROM user_constraints 
 WHERE table_name = 'CUSTOMER';
 
+--SALESMAN
+CREATE TABLE Salesman (
+    EmpNo Char(8)CONSTRAINT sal_empno_pk PRIMARY KEY,
+	EmpFirstName VarChar2(15),
+	EmpLastName VarChar2(15),
+	EmpPhone Varchar2(20),
+ 	EmpEmail Varchar2(25),
+	SupEmpNo Char(8),
+	EmpCommRate Number(4,2));
+DESCRIBE Salesman;
 
+--PRODUCT
+CREATE TABLE Product (
+    ProdNo Char(8)CONSTRAINT prod_prodno_pk PRIMARY KEY,
+	ProdName Varchar2(35),
+	ProdMfg Varchar2(20),
+	ProdQOH Number(5),
+	ProdPrice Number(7,2));
+DESCRIBE Product;
+
+--MORDER
+CREATE TABLE MOrder (
+    OrdNo Char(8) CONSTRAINT morder_ordno_pk PRIMARY KEY,
+	OrdDate Date,
+	CustNo Char(8),
+	OrdName Varchar2(30),
+	OrdStreet Varchar2(30),
+	OrdCity	Varchar2(15),
+	OrdState Char(2),
+	OrdZip Char(10),
+	EmpNo Char(8));
+DESCRIBE MOrder;
+
+--ADD FK CUSTNO
+ALTER TABLE morder add constraint ord_custno_fk
+FOREIGN KEY(custno) REFERENCES customer(custno);
+    
+--AD FK EMPNO
+ALTER TABLE morder add constraint ord_empno_fk
+FOREIGN KEY(empno) REFERENCES salesman(empno);
+
+--MENAMPILKAN CONSTRAINT
+SELECT * FROM user_constraints 
+WHERE table_name = 'MORDER';
+
+--DORDER
+CREATE TABLE DOrder(
+    Ordno Char(8),
+	ProdNo Char(8),
+	Qty Number(3));
 
